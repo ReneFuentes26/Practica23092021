539d0e8f6fb493c1c2147b1a05ca0471d15aa002
ReneFuentes26 <fuentesbenitezreneismael@gmail.com>
2021-09-24 08:43:28 -0600

    Agregar archivos de proyecto.

---
+++ Practica2309/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica2309
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Instancia de la clase Profesor
+            Professor professor = new Professor();
+            professor.setProfesorId(1);
+            professor.setTitulo("Tecnico en Ingles");
+
+            //Mostrar datos de Porfesor
+            MessageBox.Show(professor.getProfesorId() + professor.getTitulo());
+        }
+    }
+}
---
+++ Practica2309/Professor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2309
+{
+    class Professor
+    {
+        //Definir miembros de la clase
+        //propiedades|atributos|datos
+        private int _ProfesorId;
+        private string _Titulo;
+        private string _Materias;
+        private string _LugardeTrabajo;
+
+
+
+
+        //metodos de propiedad
+        //get-- obtiene el valor almacenado en la propiedad
+        //sett .. asignar un valor a la variable de propiedad
+
+
+        //ProfesorId
+        public void setProfesorId(int ProfesorId)
+        {
+            _ProfesorId = ProfesorId;
+        }
+
+        public int getProfesorId()
+        {
+            return _ProfesorId;
+        }
+
+        //Titulo
+        public void setTitulo(String titulo)
+        {
+            _Titulo = titulo;
+
+        }
+
+        public string getTitulo()
+        {
+            return _Titulo;
+        }
+
+        //Edad
+        public void setMaterias(string Materias)
+        {
+            _Materias = Materias;
+        }
+
+        public string getMaterias()
+        {
+            return _Materias;
+        }
+
+        //Lugar de Trabajo
+        public void setLugardeTrabajo(string LugardeTrabajo)
+        {
+            _LugardeTrabajo = LugardeTrabajo;
+        }
+
+        public string getLugardeTrabajo()
+        {
+            return _LugardeTrabajo;
+        }
+    }
+}
